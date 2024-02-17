529a197d1cc2b86ee12a065a1ac632920e5fa696
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-17 12:19:27 +0200

    implement methods in Page class

--- framework/page.py
+++ framework/page.py
@@ -1,10 +1,24 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 class Page:
 
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self):
-        raise NotImplementedError
+    def find_element(self, locator: tuple, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located(*locator)
+        )
 
-    def click_element(self):
-        raise NotImplementedError
+    def click_element(self, locator: tuple):
+        element = self.find_element(locator)
+        element.click()
+        return element
+
+    def insert_value(self, locator: tuple, value: str):
+        self.click_element(locator).send_keys(value)
+
+    def clean_field_value(self, locator: tuple):
+        self.click_element(locator).clear()
