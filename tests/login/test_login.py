9207bb70c08cb7272f8b54c2a3d6a9224e6d4a6b
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-17 12:32:32 +0200

    implement LoginPage class with login method

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,2 +1,21 @@
-def test_user_login(user_login_fixture):
-    assert True
+import pytest
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+
+from framework import LoginPage
+
+
+@pytest.mark.parametrize("username, password, expected_result", [
+    ("qa.ajax.app.automation@gmail.com", "qa_automation_password", True),
+    ("invalid_username", "valid_password", False),
+])
+def test_user_login(user_login_fixture: LoginPage, email: str, password: str, expected_result: bool) -> None:
+    user_login_fixture.login(email, password)
+
+    try:
+        user_login_fixture.wait_for_element(By.ID, "com.ajaxsystems:id/hubAdd")
+        button_found = True
+    except TimeoutException:
+        button_found = False
+
+    assert button_found == expected_result
