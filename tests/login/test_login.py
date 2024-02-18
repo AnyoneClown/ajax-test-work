283bad6982ae8f3c571c6bb71b1fe8dd4ededeb4
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-18 09:01:15 +0200

    add tests for sidebar components

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -25,8 +25,25 @@ def test_user_login(user_login_fixture: LoginPage, email: str, password: str, ex
     assert button_found == expected_result
 
 
+@pytest.mark.parametrize(
+    'locator, exit_button, expected_element',
+    [
+        ((AppiumBy.ID, 'com.ajaxsystems:id/settings'), (AppiumBy.ID, "com.ajaxsystems:id/back"), (AppiumBy.ID, "com.ajaxsystems:id/toolbarTitle")),
+        ((AppiumBy.ID, 'com.ajaxsystems:id/help'), (AppiumBy.ID, "com.ajaxsystems:id/back"), (AppiumBy.ID, 'com.ajaxsystems:id/navigation')),
+        ((AppiumBy.ID, 'com.ajaxsystems:id/logs'), (AppiumBy.ID, "com.ajaxsystems:id/sendButton"), (AppiumBy.ID, 'com.ajaxsystems:id/content')),
+        ((AppiumBy.ID, 'com.ajaxsystems:id/camera'), (AppiumBy.ID, "com.ajaxsystems:id/back"), (AppiumBy.ID, 'com.ajaxsystems:id/toolbarTitle')),
+        ((AppiumBy.ID, 'com.ajaxsystems:id/addHub'), (AppiumBy.ID, "com.ajaxsystems:id/backButton"), (AppiumBy.ID, 'com.ajaxsystems:id/title'))
+    ]
+)
+def test_sidebar_components(user_login_fixture, locator, exit_button, expected_element):
+    logger.info(f"Testing sidebar component with locator: {locator}, exit button: {exit_button}, expected element: {expected_element}")
+    user_login_fixture.click_element((AppiumBy.ID, 'com.ajaxsystems:id/menuDrawer'))
+    user_login_fixture.click_element(locator)
+    assert user_login_fixture.find_element(expected_element)
+    user_login_fixture.click_element(exit_button)
+
+
 def test_user_logout(user_login_fixture: LoginPage) -> None:
     logger.info("Test logging out")
     user_login_fixture.logout()
-    logo = user_login_fixture.find_element((AppiumBy.ID, "com.ajaxsystems:id/logo"))
-    assert logo
+    assert user_login_fixture.find_element((AppiumBy.ID, "com.ajaxsystems:id/logo"))
