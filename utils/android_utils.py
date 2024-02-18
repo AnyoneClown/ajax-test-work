4e99b4a604bf6116b4b6d8f2ebb0181fc63c12f7
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-18 09:10:34 +0200

    update logic of udid extraction

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -5,20 +5,15 @@ from utils.logging_config import logger
 
 def get_udid():
     try:
-        udid = subprocess.check_output(["adb", "get-serialno"]).decode("utf-8").strip()
+        udid = subprocess.run(["adb", 'devices', '-l'], shell=True, text=True, capture_output=True)
         logger.info(f"Device UDID: {udid}")
-        return udid
+        return udid.stdout.split()[0]
     except Exception as e:
         logger.error("Failed to get device UDID", exc_info=True)
-        return None
+        return "adb-414e7250-OZJrqe._adb-tls-connect._tcp"
 
 
 def android_get_desired_capabilities():
-    udid = get_udid()
-
-    if not udid:
-        udid = "adb-414e7250-OZJrqe._adb-tls-connect._tcp"
-
     return {
         "autoGrantPermissions": True,
         "automationName": "uiautomator2",
@@ -29,7 +24,7 @@ def android_get_desired_capabilities():
         "resetKeyboard": True,
         "systemPort": 8301,
         "takesScreenshot": True,
-        "udid": udid,
+        "udid": get_udid(),
         "appPackage": "com.ajaxsystems",
         "appActivity": "com.ajaxsystems.ui.activity.LauncherActivity",
     }
