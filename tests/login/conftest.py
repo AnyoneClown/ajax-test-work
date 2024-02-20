64e0f8b9d77b9465e16612614540266305cf2520
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-20 20:42:45 +0200

    create login_once fixture for sidebar tests

--- tests/login/conftest.py
+++ tests/login/conftest.py
@@ -6,3 +6,8 @@ from framework.login_page import LoginPage
 @pytest.fixture(scope="function")
 def user_login_fixture(driver):
     yield LoginPage(driver)
+
+
+@pytest.fixture(scope="module")
+def login_once(driver):
+    LoginPage(driver).login("qa.ajax.app.automation@gmail.com", "qa_automation_password")
